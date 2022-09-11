4388f5793548ddc78eb74871f1544abb4af216df
Achelia Selim <73719319+aselim21@users.noreply.github.com>
2022-09-11 18:55:12 +0200

    tixed the links for github pages to test

--- src/_includes/default.11ty.js
+++ src/_includes/default.11ty.js
@@ -13,7 +13,7 @@ exports.render = function (pageData) {
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width,initial-scale=1.0">
       <title>PinkBank | ${data.title}</title>
-      <link rel="stylesheet" href="./assets/styles/compiled_styles/main.css">
+      <link rel="stylesheet" href="../../assets/styles/compiled_styles/main.css">
       <link rel="preconnect" href="https://fonts.googleapis.com">
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
       <link href="https://fonts.googleapis.com/css2?family=Graduate&display=swap" rel="stylesheet">
@@ -28,7 +28,7 @@ exports.render = function (pageData) {
             ${footer.html}
           </section>
         </main>
-    <script src="/.assets/scripts/main.js"></script>
+    <script src="../../assets/scripts/main.js"></script>
   </body>
   </html>
   `;
